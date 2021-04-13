039d68e702225d94dafc08f82a4989571f77224e
woodc555 <>
2021-04-13 16:55:11 +0000

    message to Message

--- message.js
+++ message.js
@@ -2,7 +2,7 @@ class Message {
    constructor(name, commands) {
      this.name = name;
      if(!name){
-       throw Error("message name required.")
+       throw Error("Message name required.")
      }
      this.commands = commands;
    }
--- rover.js
+++ rover.js
@@ -1,10 +1,9 @@
 class Rover {
-   constructor (position) {
+   constructor(position) {
      this.position = position;
      this.mode = 'NORMAL';
      this.generatorWatts = 110;
-   };
-
+   }
    receiveMessage(message) {
      let resultsArray = [];
      for (let command of message.commands) {
@@ -12,24 +11,24 @@ class Rover {
          if (this.mode !== 'LOW_POWER') {
            this.position = command.value;
            resultsArray.push({completed: true});
-         }
+         } 
          else {
            resultsArray.push({completed: false});
-         }
-       }
-       else if (command.commandType === 'STATUS_CHECK'){
-         let mode = this.mode;
-         let generatorWatts = this.generatorWatts;
-         let currentPosition = this.position;
-         resultsArray.push({comleted: true, roverStatus: {mode: mode, generatorWatts: generatorWatts, position: currentPosition}});
-       }
+         }              
+       } 
+       else if (command.commandType === 'STATUS_CHECK') {
+        let mode = this.mode;
+        let generatorWatts = this.generatorWatts;
+        let currentPosition = this.position;
+         resultsArray.push({completed: true, roverStatus: {mode: mode, generatorWatts: generatorWatts, position: currentPosition}});
+       } 
        else if (command.commandType === 'MODE_CHANGE') {
-         if (this.mode !== command.value){
+         if (this.mode !== command.value) {
            this.mode = command.value;
            resultsArray.push({completed: true});
-         }
-         else {
-           resultsArray.push({comleted: false});
+         } 
+         else {         
+         resultsArray.push({completed: false});
          }
        }
      }
